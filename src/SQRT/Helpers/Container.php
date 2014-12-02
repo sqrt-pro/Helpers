b99114ba5a71fe30c15aacffd3f5c75dfb18501f
Igor <igor@osharin.ru>
2014-12-02 08:42:55 +0300

    Container

---
+++ src/SQRT/Helpers/Container.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace SQRT\Helpers;
+
+class Container implements \ArrayAccess
+{
+  protected $vars;
+
+  function __get($name)
+  {
+    return $this->get($name);
+  }
+
+  function __set($name, $value)
+  {
+    return $this->set($name, $value);
+  }
+
+  /** Получить значение */
+  public function get($name, $default = false)
+  {
+    return $this->has($name)
+      ? $this->vars[$name]
+      : $default;
+  }
+
+  /**
+   * Установить значение
+   * Если $value = null, значение удаляется
+   */
+  public function set($name, $value)
+  {
+    if (is_null($value)) {
+      unset($this->vars[$name]);
+    } else {
+      $this->vars[$name] = $value;
+    }
+
+    return $this;
+  }
+
+  /** Проверка, установлено ли значение */
+  public function has($name)
+  {
+    return isset($this->vars[$name]);
+  }
+
+  /** Добавить значение в конец строки или массива */
+  public function append($name, $value)
+  {
+    $v = $this->get($name);
+
+    if (is_array($v)) {
+      array_push($v, $value);
+
+      return $this->set($name, $v);
+    }
+
+    return $this->set($name, $v . $value);
+  }
+
+  /** Добавить значение в начало строки или массива */
+  public function prepend($name, $value)
+  {
+    $v = $this->get($name);
+
+    if (is_array($v)) {
+      array_unshift($v, $value);
+
+      return $this->set($name, $v);
+    }
+
+    return $this->set($name, $value . $this->get($name));
+  }
+
+  /** Все значения в виде массива */
+  public function toArray()
+  {
+    return $this->vars;
+  }
+
+  /** Загрузка значений из массива. $clear - затереть существующие данные */
+  public function fromArray(array $array, $clear = false)
+  {
+    if ($clear) {
+      $this->vars = array();
+    }
+
+    foreach ($array as $key => $val) {
+      $this->set($key, $val);
+    }
+
+    return $this;
+  }
+
+  public function offsetExists($offset)
+  {
+    return $this->has($offset);
+  }
+
+  public function offsetGet($offset)
+  {
+    return $this->get($offset);
+  }
+
+  public function offsetSet($offset, $value)
+  {
+    $this->set($offset, $value);
+  }
+
+  public function offsetUnset($offset)
+  {
+    $this->set($offset, null);
+  }
+}
